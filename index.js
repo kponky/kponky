afc71164d36dcd997c5aed2c34f1b4bd96746276
kponky <ikponkedet@gamil.com>
2022-04-14 12:52:09 +0100

    message

---
+++ index.js
@@ -0,0 +1,127 @@
+// let i = 3;
+// while (i) {
+// alert( i-- );
+// }
+
+// the last value is 1 because 3 is the number and (-- number is been subtractred by one n 1 is the last number)
+// because your are decrementing the value of i
+
+// Ques. which value does the while loop show
+
+// they are of the same value
+
+// let i = 0;
+// while (++i < 5)
+// alert ( i );
+
+
+// let i = 0;
+// while (i++ < 5) alert (i);
+
+
+// for ( let i = 0; i < 5; i++) 
+// alert (i);
+
+
+// for (let i = 0; i++){
+//     alert(i);
+// }
+
+
+// for (let number=2; number <= 10; number++){
+//     if (number % 2 == 0){
+//         console.log(number)
+//     }
+// }
+
+
+// let i= 0;
+// while (i++ < 3)
+// alert(`number is ${i}!`);
+
+
+
+// let number;
+
+// do{
+//     number = prompt("Enter a number graeter than 100?", 0);
+// }while (number <= 100 && number);
+
+// let  num = +prompt ('Enter a number greater than 100', " ");
+// if (num > 100){
+//     console.log(`number is ${num}`)
+// }else{
+//     +prompt('please input again')
+// }
+    
+// let num = +prompt('put a number greater than 100', " ")
+// label:
+// while (num){
+//     if (num < 100){
+//         prompt('input again')
+//     }else if (num > 100){
+//         alert(num)   }
+// }
+
+        
+    
+
+
+
+
+
+// let n = 10;
+// nextPrime:
+// for (let i =2; i <= n; i++){
+
+//     for (let j = 2; j < i; j++){
+//         if (i % j == 0) continue nextPrime;
+//     }
+//     alert( i );
+
+
+// let primeNumber = 10;
+// outerloop:                                          
+// // label
+// for (let x=2; x <= primeNumber; x++){
+//         // console.log(x)
+//         for(let y = 2; y < x; y++){
+//         if(x % y == 0) continue outerloop;    
+// }
+// // console.log(x)
+// }
+
+// let primeNumber = 10;
+// outerloop: 
+// for(let X = 2; x <= primeNumber; x++){
+//     console.log(`x1  is ${x}`)
+//     for(let y = 2; y < X; y++){
+//         console.log(`x2 is `)
+
+
+
+// //          }
+    
+// }
+
+
+
+//   function 
+
+
+// function primeNumbers(num){
+//     outerloop: 
+//     for(let x = 2; x < num; x++){
+//         for (let y = 2; y < x; y++){
+//             if (x % y == 0)continue outerloop;
+//         }
+//         console.log(x)
+//     }
+// }
+// primeNumbers(20)
+
+
+
+
+
+
